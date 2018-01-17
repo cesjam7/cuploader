e3d14dba580f71854ca8ae34bb04b9ce5c593ce9
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2018-01-17 17:33:14 -0500

    first commit

---
+++ cuploader.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.2.0/min/dropzone.min.css">
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.2.0/min/dropzone.min.js"></script>
+
+<form action="cuploader.php" class="dropzone">
+
+</form>
